6de684788b98f64447dbc7b0516cfdfc9a732215
Krzyzu <kruchy61@gmail.com>
2023-09-11 15:29:47 +0200

    Revert DatabaseHelper which surprisingly disappeared

--- app/src/main/java/com/mypharma/registry/DatabaseHelper.kt
+++ app/src/main/java/com/mypharma/registry/DatabaseHelper.kt
@@ -2,74 +2,91 @@ package com.mypharma.registry
 
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
-import android.database.sqlite.SQLiteOpenHelper
-import java.io.BufferedReader
-import java.io.InputStreamReader
+import com.fasterxml.jackson.dataformat.csv.CsvMapper
+import com.fasterxml.jackson.dataformat.csv.CsvParser
+import com.fasterxml.jackson.dataformat.csv.CsvSchema
+import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper
+import com.j256.ormlite.dao.Dao
+import com.j256.ormlite.support.ConnectionSource
+import com.j256.ormlite.table.TableUtils
+import com.mypharma.medicine.Drug
+import java.io.InputStream
+import java.sql.SQLException
 
-class DatabaseHelper(context: Context) :
-    SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
 
-    private val mContext: Context = context
+class DatabaseHelper(
+    context: Context,
+    val inputStream: InputStream
+) : OrmLiteSqliteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    private var drugDao: Dao<Drug, Long>? = null
+
 
     companion object {
         internal const val DATABASE_NAME = "medications.db"
         private const val DATABASE_VERSION = 1
     }
 
-    override fun onCreate(db: SQLiteDatabase) {
-        val createTableSQL = """
-            CREATE TABLE medications (
-                id INTEGER PRIMARY KEY,
-                product_name TEXT,
-                common_name TEXT,
-                strength TEXT,
-                form TEXT,
-                atc_code TEXT,
-                responsible_entity TEXT,
-                packaging TEXT,
-                active_substance TEXT,
-                leaflet TEXT,
-                characteristics TEXT
-            )
-        """
-        db.execSQL(createTableSQL)
-
-        loadCSVtoDatabase(db)
-    }
-
-    private fun loadCSVtoDatabase(db: SQLiteDatabase) {
-        val inputStream = mContext.assets.open("Registry.csv")
-        val bufferedReader = BufferedReader(InputStreamReader(inputStream))
-        bufferedReader.readLine()
-
-        var line: String?
-        while (bufferedReader.readLine().also { line = it } != null) {
-            val tokens = line?.split(",") ?: continue
-            val insertSQL = """
-                INSERT INTO medications (id, product_name, common_name, strength, form, atc_code, responsible_entity, packaging, active_substance, leaflet, characteristics)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-            """
-            db.execSQL(
-                insertSQL,
-                arrayOf(
-                    tokens[0],
-                    tokens[1],
-                    tokens[2],
-                    tokens[3],
-                    tokens[4],
-                    tokens[5],
-                    tokens[6],
-                    tokens[7],
-                    tokens[8],
-                    tokens[9],
-                    tokens[10]
-                )
-            )
+    override fun onCreate(db: SQLiteDatabase, connectionSource: ConnectionSource) {
+        try {
+            TableUtils.createTable(connectionSource, Drug::class.java)
+            loadCSVtoDatabase()
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
-        bufferedReader.close()
     }
 
-    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
-        onCreate(db)
+    private fun loadCSVtoDatabase() {
+        val csvMapper = CsvMapper().apply {
+            enable(CsvParser.Feature.TRIM_SPACES)
+            enable(CsvParser.Feature.SKIP_EMPTY_LINES)
+        }
+        val schema = CsvSchema.builder().addNumberColumn("Identyfikator Produktu Leczniczego")
+            .addColumn("Nazwa Produktu Leczniczego")
+            .addColumn("Nazwa powszechnie stosowana")
+            .addColumn("Moc")
+            .addColumn("Postać farmaceutyczna")
+            .addColumn("Kod ATC")
+            .addColumn("Podmiot odpowiedzialny")
+            .addColumn("Opakowanie")
+            .addColumn("Substancja czynna")
+            .addColumn("Ulotka")
+            .addColumn("Charakterystyka")
+            .build()
+
+        csvMapper.readerFor(Drug::class.java)
+            .with(schema.withSkipFirstDataRow(true))
+            .readValues<Drug>(inputStream)
+            .readAll()
+            .map { getDao()!!.create(it) }
+
+
     }
+
+    override fun onUpgrade(
+        db: SQLiteDatabase,
+        connectionSource: ConnectionSource,
+        oldVersion: Int,
+        newVersion: Int
+    ) {
+        try {
+            TableUtils.dropTable<Drug, Int>(connectionSource, Drug::class.java, false)
+            onCreate(db, connectionSource)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    @Throws(SQLException::class)
+    fun getDao(): Dao<Drug, Long>? {
+        if (drugDao == null) {
+            try {
+                drugDao = getDao(Drug::class.java)
+            } catch (e: SQLException) {
+                e.printStackTrace()
+            }
+        }
+        return drugDao
+    }
+
 }
